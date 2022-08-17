ea6da5a53135ebded6dc7020d9f25f53182fd287
2BeAsh <thefatfishmclol@gmail.com>
2022-08-17 18:52:09 +0200

    Update test.py
    Module test

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 #%% Imports
 import numpy as np
 import matplotlib.pyplot as plt
+from swarm import prey_prey_deriv
 
 #%% Test Graph
 x = np.arange(0, 10, 1)
@@ -12,3 +13,8 @@ plt.show()
 #%%
 
 print("hej")
+
+
+#%%
+
+print(prey_prey_deriv(t=1, x_vec=(np.arange(10), np.arange(10)), a=1))
