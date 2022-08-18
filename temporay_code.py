fc1609b4dc1f699c39e55352899017167c4a4f18
2BeAsh <thefatfishmclol@gmail.com>
2022-08-18 11:57:58 +0200

    update on force func

--- temporay_code.py
+++ temporay_code.py
@@ -3,24 +3,35 @@ import matplotlib.pyplot as plt
 
 
 
-def force_no_loop(t, coord, a, b, c, p):
-    x, y = coord
+def force_no_loop(t, prey_coord, pred_coord, a, b, c, p):    
+    x, y = prey_coord
+    zx, zy = pred_coord
     N = x.size
     
     xk = x[:, None]
     xj = x[None, :]
-    xjk = xj - xk
-    
+
     yk = y[:, None]
     yj =y[None, :]
 
+    dist_xy_p2 = np.sum((xj - xk)**2 + (yj - yk)**2, axis=1)
+    dist_prey_pred_p = np.sqrt((x - zx)**2 + (y - zy)**2)
     
-    dist_xy_p2 = np.sum((xj - yj)**2 + (xk - yk)**2, axis=1)
-    #dist_xz_p2 = np.sum((xj - zj)**2 + (xj - zj)**2, axis=1)
+    fx = 1 / N * (np.sum(xj - xk, axis=1) / dist_xy_p2 
+                  - a * np.sum(xj - xk, axis=1) 
+                  + b * (x - zx) / dist_prey_pred_p**2)
     
-    fx = 1 / N * (np.sum(xj - xk, axis=1) / dist_xy_p2 - a * (np.sum(xj - xk, axis=1)))
-    fy = 1 / N * (np.sum(yj - yk, axis=1) / dist_xy_p2 - a * (np.sum(yj - yk, axis=1)))
-    return fx, fy
+    fy = 1 / N * (np.sum(yj - yk, axis=1) / dist_xy_p2
+                  - a * np.sum(yj - yk, axis=1)
+                  + b * (y - zy) / dist_prey_pred_p**2)
+    
+    fzx = c / N * np.sum((x - zx) / dist_prey_pred_p**p)
+    fzy = c / N * np.sum((y - zy) / dist_prey_pred_p**p)
+    return fx, fy, fzx, fzy
 
 x = np.arange(10)
-print(force_no_loop(t=1, coord=(x,x), a=1,b=1,c=1,p=1))
+y = np.array([2,3,5,6,7,8,9,1,10,2])
+
+#print(x)
+#print(y)
+print(force_no_loop(t=1, prey_coord=(x,y), pred_coord=(1,1), a=1,b=1,c=1,p=1))
