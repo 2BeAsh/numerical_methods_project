6520dd413c3f87780ecc128b6a41295d4bd10d11
2BeAsh <thefatfishmclol@gmail.com>
2022-08-18 14:20:36 +0200

    Update swarm.py
    force_no_loop works now, wrongly divided by N on all terms

--- swarm.py
+++ swarm.py
@@ -87,29 +87,32 @@ def force_no_loop(t, coord, a, b, c, p):
     yj = y[:, None]
     yk =y[None, :]
 
-    dist_prey_pred_p = np.sqrt((x - zx)**2 + (y - zy)**2)
-    dist_xy_p2 = (xj - xk)**2 + (yj - yk)**2 
+    dist_prey_pred = (x - zx)**2 + (y - zy)**2
+    dist_xy_p2     = (xj - xk)**2 + (yj - yk)**2 
     
     # Get x and y specific, set terms with distance 0 equal to 0
-    div_term_x = np.where(dist_xy_p2 == 0, 0, (xj - xk) / dist_xy_p2)
-    div_term_y = np.where(dist_xy_p2 == 0, 0, (yj - yk) / dist_xy_p2)
+    div_term_x = np.divide(xj - xk, dist_xy_p2, out=np.zeros((N,N), dtype=float), where=dist_xy_p2!=0)
+    div_term_y = np.divide(yj - yk, dist_xy_p2, out=np.zeros((N,N), dtype=float), where=dist_xy_p2!=0)
+
+    #div_term_x = np.where(dist_xy_p2 == 0, 0, (xj - xk) / dist_xy_p2)
+    #div_term_y = np.where(dist_xy_p2 == 0, 0, (yj - yk) / dist_xy_p2)
     
-    fx = 1 / N * (np.sum(div_term_x - a * (xj - xk), axis=1)
-                  + b * (x - zx) / dist_prey_pred_p**2)
+    fx = 1 / N * np.sum(div_term_x - a * (xj - xk), axis=1) \
+                  + b * (x - zx) / dist_prey_pred
     
-    fy = 1 / N * (np.sum(div_term_y - a * (yj - yk), axis=1)
-                  + b * (y - zy) / dist_prey_pred_p**2)
+    fy = 1 / N * np.sum(div_term_y - a * (yj - yk), axis=1) \
+                  + b * (y - zy) / dist_prey_pred
     
-    fzx = c / N * np.sum((x - zx) / dist_prey_pred_p**p)
-    fzy = c / N * np.sum((y - zy) / dist_prey_pred_p**p)
+    fzx = c / N * np.sum((x - zx) / dist_prey_pred**(p/2)) # Divide p by 2 because normally squared
+    fzy = c / N * np.sum((y - zy) / dist_prey_pred**(p/2))
     return fx, fy, fzx, fzy
 
 
 #%% Test if force function works on simple example
 prey_coord = [np.arange(16), np.arange(16)]
 pred_coord = np.array([0.5,0.1])
-print(force(t=1, coord=(prey_coord, pred_coord), a=1, b=1, c=2, p=3)[-2].size)
-print(force_no_loop(t=1, coord=(prey_coord, pred_coord), a=1, b=1, c=2, p=3)[-2].size)
+#print(force(t=1, coord=(prey_coord, pred_coord), a=1, b=1, c=2, p=3)[1])
+#print(force_no_loop(t=1, coord=(prey_coord, pred_coord), a=1, b=1, c=2, p=3)[1])
 
 #%% Create movement of prey and predator
 def movement(N, L, t_end, dt, a, b, c, p):
@@ -149,7 +152,7 @@ def movement(N, L, t_end, dt, a, b, c, p):
         # Get values
         prey_coord = [x, y]
         predator_coord = [zx, zy]
-        fx, fy, fzx, fzy = force(t=t, coord=(prey_coord, predator_coord), a=a, b=b, c=c, p=p)
+        fx, fy, fzx, fzy = force_no_loop(t=t, coord=(prey_coord, predator_coord), a=a, b=b, c=c, p=p)
         # Update values
         x = x + fx * dt
         y = y + fy * dt
