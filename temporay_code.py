1a5d5762f17ece597d6d105776706bce1ea31f37
2BeAsh <thefatfishmclol@gmail.com>
2022-08-18 13:37:33 +0200

    Implemented force_no_loop in Swarm
    Update force_no_loop

--- temporay_code.py
+++ temporay_code.py
@@ -41,25 +41,26 @@ def force_no_loop_combined(t, prey_coord, pred_coord, a, b, c, p):
     yk = y[:, None]
     yj =y[None, :]
 
-    dist_xy_p2 = (xj - xk)**2 + (yj - yk)**2
     dist_prey_pred_p = np.sqrt((x - zx)**2 + (y - zy)**2)
+
+    dist_xy_p2 = (xj - xk)**2 + (yj - yk)**2        
+    div_term_x = np.where(dist_xy_p2 == 0, 0, (xj - xk) / dist_xy_p2) # Set terms with distance = 0 to 0
+    div_term_y = np.where(dist_xy_p2 == 0, 0, (yj - yk) / dist_xy_p2)
     
-    fx = 1 / N * (np.sum((xj - xk) / dist_xy_p2 
-                  - a * (xj - xk), axis=1)
+    fx = 1 / N * (np.sum(div_term_x - a * (xj - xk), axis=1)
                   + b * (x - zx) / dist_prey_pred_p**2)
     
-    fy = 1 / N * (np.sum(yj - yk, axis=1) / dist_xy_p2
-                  - a * np.sum(yj - yk, axis=1)
+    fy = 1 / N * (np.sum(div_term_y - a * (yj - yk), axis=1)
                   + b * (y - zy) / dist_prey_pred_p**2)
     
     fzx = c / N * np.sum((x - zx) / dist_prey_pred_p**p)
     fzy = c / N * np.sum((y - zy) / dist_prey_pred_p**p)
     return fx, fy, fzx, fzy
 
-
+#%%
 x = np.arange(10)
 y = np.array([2,3,5,6,7,8,9,1,10,2])
-
 #print(x)
 #print(y)
 print(force_no_loop_combined(t=1, prey_coord=(x,y), pred_coord=(1,1), a=1,b=1,c=1,p=1))
+
