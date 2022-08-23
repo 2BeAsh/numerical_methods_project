f94e5af475dbeb81bbbdffa25f8fe3124ed6f1cc
2BeAsh <thefatfishmclol@gmail.com>
2022-08-23 08:40:27 +0200

    Separated Raycasting movement
    Removed raycasting lines from sheep movement as they did not work. Stored them in new script that is not used anywhere

--- PredatorPreyGame/Assets/SheepMove.cs
+++ PredatorPreyGame/Assets/SheepMove.cs
@@ -28,73 +28,39 @@ public class SheepMove : MonoBehaviour
         Loop through each member and calculate their velocity, and from that update their position.
         Calculation requires looping through all members again, excluding the first. */
 
-        // Find all current prey
+        // Find all current prey and add them to the list preyList
         List<GameObject> preyList = new();
         foreach (GameObject fooObj in GameObject.FindGameObjectsWithTag("Prey"))  //Maybe find a way to only run this code if an object is removed
         {
             preyList.Add(fooObj);
         }
-
+        // Loop through each prey in preyList and update its position by calculating its velocity
         foreach (GameObject prey in preyList)
         {
             Vector2 velocity = Move_prey(preyList, prey);
 
-            // Use Raycast to check if there is a collision object
-            if (checkCollision(layer, prey, velocity))
+            // Align sprite direction with velocity
+            SpriteRenderer spriteRenderer = prey.GetComponent<SpriteRenderer>();
+            if (velocity.x < 0)
             {
-                continue; // If there is a collision, don't update position of this prey - Might want to make a while loop that continues until gets true
+                spriteRenderer.flipX = true;
             }
-            else
+            else if (velocity.x > 0)
             {
-                // Align sprite direction with velocity
-                SpriteRenderer spriteRenderer = prey.GetComponent<SpriteRenderer>();
-                if (velocity.x < 0)
-                {
-                    spriteRenderer.flipX = true;
-                }
-                else if (velocity.x > 0)
-                {
-                    spriteRenderer.flipX = false;
-                }
-
-                // Movement
-                Rigidbody2D preyRB = prey.GetComponent<Rigidbody2D>(); // Get j'th particle's rigidbody and update its position
-                preyRB.MovePosition(preyRB.position + ((Vector2)velocity * Time.fixedDeltaTime)); // Changed to fixedDeltaTime since using FixedUpdate for rigid body
-
+                spriteRenderer.flipX = false;
             }
 
-
-            /*
-            // Check for collisions
+            // Movement
             Rigidbody2D preyRB = prey.GetComponent<Rigidbody2D>(); // Get j'th particle's rigidbody and update its position
-            List<RaycastHit2D> castCollisions = new List<RaycastHit2D>();
-            float velocityNorm = (velocity.x * velocity.x) + (velocity.y * velocity.y);
-            int count = preyRB.Cast(
-                velocity,
-                movementFilter,
-                castCollisions,
-                velocityNorm * Time.fixedDeltaTime + collisionOffset);
-
-            if (count == 0)
-            {
-                preyRB.MovePosition(preyRB.position + (Vector2)velocity * Time.fixedDeltaTime);
-            }
-            else if (count != 0)
-            {
-                Debug.Log("collision");
-            }
-            */
-            // Update position from velocity calculations
-            //prey.transform.position += velocity * Time.fixedDeltaTime;  - Doesn't allow for physics
+            preyRB.MovePosition(preyRB.position + (velocity * Time.fixedDeltaTime)); // Changed to fixedDeltaTime since using FixedUpdate for rigid body
 
         }
 
     }
 
-
     private Vector2 Move_prey(List<GameObject> preyList, GameObject prey)
     {       int numberOfPrey = preyList.Count;
-            Vector3 velocity = new Vector3(0, 0, 0); // Velocity of j'th particle, will be updated
+            Vector2 velocity = new Vector2(0, 0); // Velocity of j'th particle, will be updated
             foreach (GameObject otherPrey in preyList) // The k'th particle
             {
                 if (otherPrey == prey)
@@ -102,15 +68,15 @@ public class SheepMove : MonoBehaviour
                     continue;
                 }
 
-                float dist = Vector3.Distance(prey.transform.position, otherPrey.transform.position);
-                Vector3 velocityShortRange = (prey.transform.position - otherPrey.transform.position) / (dist * dist);
-                Vector3 velocityLongRange = longRangeAttraction * (prey.transform.position - otherPrey.transform.position);
+                float dist = Vector2.Distance(prey.transform.position, otherPrey.transform.position);
+                Vector2 velocityShortRange = (prey.transform.position - otherPrey.transform.position) / (dist * dist);
+                Vector2 velocityLongRange = longRangeAttraction * (prey.transform.position - otherPrey.transform.position);
 
                 velocity += (velocityShortRange - velocityLongRange) / numberOfPrey;
 
             }
-            float dist_pred = Vector3.Distance(prey.transform.position, playerObj.transform.position);
-            Vector3 velocityPredRepulsion = predatorRepulsion * (prey.transform.position - playerObj.transform.position) / (dist_pred * dist_pred);
+            float dist_pred = Vector2.Distance(prey.transform.position, playerObj.transform.position);
+            Vector2 velocityPredRepulsion = predatorRepulsion * (prey.transform.position - playerObj.transform.position) / (dist_pred * dist_pred);
             velocity += velocityPredRepulsion;
         return velocity;
     }
