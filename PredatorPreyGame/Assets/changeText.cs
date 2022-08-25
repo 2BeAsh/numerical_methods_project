706f37225f5281c16351d98da07cacc1e86b55d3
2BeAsh <thefatfishmclol@gmail.com>
2022-08-25 13:28:59 +0200

    character selection

--- PredatorPreyGame/Assets/changeText.cs
+++ PredatorPreyGame/Assets/changeText.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class changeText : MonoBehaviour
 {
+
+    private TMP_Text textContent;
+    public string TextOnHover;
+    private Image ImageObj;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        textContent = GetComponentInChildren<TMP_Text>();
+        ImageObj = GetComponent<Image>();
+
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void OnMouseOver()
     {
-        
+        textContent.text = TextOnHover;
+        ImageObj.color = Color.red;
     }
+
+
 }
